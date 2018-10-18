fb872d1fe7bad3ce5e874ea9ca503c32f0c69911
prokajkristof <kiscoloshun@gmail.com>
2018-10-18 21:03:30 +0200

    sanity archiver

---
+++ SanityArchiver/SanityArchiver/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace SanityArchiver
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.driveList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Size1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Date1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Compress = new System.Windows.Forms.Button();
+            this.crypt = new System.Windows.Forms.Button();
+            this.DeCompress = new System.Windows.Forms.Button();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // driveList
+            // 
+            this.driveList.FormattingEnabled = true;
+            this.driveList.Location = new System.Drawing.Point(12, 55);
+            this.driveList.Name = "driveList";
+            this.driveList.Size = new System.Drawing.Size(138, 56);
+            this.driveList.TabIndex = 3;
+            this.driveList.SelectedIndexChanged += new System.EventHandler(this.driveList_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(115, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Size1,
+            this.Date1});
+            this.listView1.Location = new System.Drawing.Point(156, 55);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(620, 264);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            this.Name.Width = 289;
+            // 
+            // Size1
+            // 
+            this.Size1.Text = "Size";
+            this.Size1.Width = 118;
+            // 
+            // Date1
+            // 
+            this.Date1.Text = "Date";
+            this.Date1.Width = 207;
+            // 
+            // Compress
+            // 
+            this.Compress.Location = new System.Drawing.Point(40, 137);
+            this.Compress.Name = "Compress";
+            this.Compress.Size = new System.Drawing.Size(75, 23);
+            this.Compress.TabIndex = 8;
+            this.Compress.Text = "Compress";
+            this.Compress.UseVisualStyleBackColor = true;
+            this.Compress.Click += new System.EventHandler(this.Compress_Click_1);
+            // 
+            // crypt
+            // 
+            this.crypt.Location = new System.Drawing.Point(40, 194);
+            this.crypt.Name = "crypt";
+            this.crypt.Size = new System.Drawing.Size(75, 23);
+            this.crypt.TabIndex = 9;
+            this.crypt.Text = "Crypt";
+            this.crypt.UseVisualStyleBackColor = true;
+            this.crypt.Click += new System.EventHandler(this.crypt_Click);
+            // 
+            // DeCompress
+            // 
+            this.DeCompress.Location = new System.Drawing.Point(40, 166);
+            this.DeCompress.Name = "DeCompress";
+            this.DeCompress.Size = new System.Drawing.Size(75, 23);
+            this.DeCompress.TabIndex = 10;
+            this.DeCompress.Text = "DeCompress";
+            this.DeCompress.UseVisualStyleBackColor = true;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(40, 223);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 11;
+            this.Delete.Text = "Delete";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(788, 332);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.DeCompress);
+            this.Controls.Add(this.crypt);
+            this.Controls.Add(this.Compress);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.driveList);
+            this.Text = "SanityArchiver";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ListBox driveList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Size1;
+        private System.Windows.Forms.ColumnHeader Date1;
+        private System.Windows.Forms.Button Compress;
+        private System.Windows.Forms.Button crypt;
+        private System.Windows.Forms.Button DeCompress;
+        private System.Windows.Forms.Button Delete;
+    }
+}
+
---
+++ SanityArchiver/SanityArchiver/Form1.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Compression;
+using System.Security.Cryptography;
+
+namespace SanityArchiver
+{
+    public partial class Form1 : Form
+    {
+        private DirectoryInfo baseDirectory = null;
+        private string file = null;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            listView1.MouseDoubleClick += new MouseEventHandler(listView1_MouseDoubleClick);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if(baseDirectory == null)
+            {
+                button1.Enabled = false;
+            }
+            foreach(DriveInfo di in DriveInfo.GetDrives())
+            {
+                driveList.Items.Add(di);
+            }
+        }
+
+        private void driveList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            
+            try
+            {
+                DriveInfo drive = (DriveInfo)driveList.SelectedItem;
+                baseDirectory = drive.RootDirectory;
+                foreach(DirectoryInfo dirInfo in drive.RootDirectory.GetDirectories())
+                {
+                    listView1.Items.Add(dirInfo.ToString());
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (baseDirectory != null)
+            {
+                String currentDirectory = baseDirectory.FullName;
+                DirectoryInfo currentDirectoryInfo = new DirectoryInfo(currentDirectory);
+                if (currentDirectoryInfo.Parent != null && baseDirectory != null)
+                {
+                    String grandParentPath = currentDirectoryInfo.Parent.FullName;
+                    listView1.Items.Clear();
+                    DirectoryInfo dir = new DirectoryInfo(grandParentPath);
+                    label1.Text = dir.FullName;
+                    foreach (DirectoryInfo di in dir.GetDirectories())
+                    {
+                        ListViewItem item = new ListViewItem(di.ToString());
+                        item.SubItems.Add("");
+                        item.SubItems.Add(File.GetCreationTime(di.FullName).ToString());
+                        listView1.Items.Add(item);
+                    }
+                    foreach (FileInfo fi in dir.GetFiles())
+                    {
+                        ListViewItem item = new ListViewItem(fi.ToString());
+                        item.SubItems.Add(fi.Length.ToString());
+                        item.SubItems.Add(File.GetCreationTime(fi.FullName).ToString());
+                        listView1.Items.Add(item);
+                    }
+                    baseDirectory = new DirectoryInfo(grandParentPath);
+                }
+                else
+                {
+                    button1.Enabled = false;
+                }
+            }
+            else
+            {
+                button1.Enabled = false;
+            }
+        }
+
+
+
+        void listView1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            ListViewHitTestInfo info = listView1.HitTest(e.X, e.Y);
+
+            if (info.Item != null)
+            {
+                DirectoryInfo dir = null;
+                foreach (DirectoryInfo dirInf in baseDirectory.GetDirectories())
+                {
+                    if (dirInf.ToString() == info.SubItem.Text)
+                    {
+                        baseDirectory = dirInf;
+                        dir = dirInf;
+                    }
+                }
+                label1.Text = baseDirectory.FullName;
+                Directory(dir);
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            String text = listView1.SelectedItems[0].Text;
+            file = text;
+        }
+
+        private void Compress_Click(object sender, EventArgs e)
+        {
+
+            
+        }
+
+        private void Compress_File(string path)
+        {
+            FileStream sourceFile = File.OpenRead(path);
+            FileStream destinationFile = File.Create(path + ".gz");
+
+            byte[] buffer = new byte[sourceFile.Length];
+            sourceFile.Read(buffer, 0, buffer.Length);
+
+            using (GZipStream output = new GZipStream(destinationFile,
+                CompressionMode.Compress))
+            {
+                Console.WriteLine("Compressing {0} to {1}.", sourceFile.Name,
+                    destinationFile.Name, false);
+
+                output.Write(buffer, 0, buffer.Length);
+            }
+
+            sourceFile.Close();
+            destinationFile.Close();
+        }
+
+        private void Compress_Click_1(object sender, EventArgs e)
+        {
+            foreach (DirectoryInfo dirInf in baseDirectory.GetDirectories())
+            {
+                if (dirInf.ToString() == file)
+                {
+                    Compress_File(dirInf.FullName);
+                    Directory(baseDirectory);
+                }
+            }
+
+            foreach (FileInfo fi in baseDirectory.GetFiles())
+            {
+                if (fi.ToString() == file)
+                {
+                    Compress_File(fi.FullName);
+                    Directory(baseDirectory);
+                }
+            }
+        }
+
+        private void Directory(DirectoryInfo dir)
+        {
+            listView1.Items.Clear();
+            foreach (DirectoryInfo di in dir.GetDirectories())
+            {
+                ListViewItem item = new ListViewItem(di.ToString());
+                item.SubItems.Add("");
+                item.SubItems.Add(File.GetCreationTime(di.FullName).ToString());
+                listView1.Items.Add(item);
+            }
+            foreach (FileInfo fi in dir.GetFiles())
+            {
+                ListViewItem item = new ListViewItem(fi.ToString());
+                item.SubItems.Add(fi.Length.ToString());
+                item.SubItems.Add(File.GetCreationTime(fi.FullName).ToString());
+                listView1.Items.Add(item);
+            }
+            button1.Enabled = true;
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            foreach (FileInfo fi in baseDirectory.GetFiles())
+            {
+                if (fi.ToString() == file)
+                {
+                    System.IO.File.Delete(fi.FullName);
+                    Directory(baseDirectory);
+                }
+            }
+
+            foreach (DirectoryInfo dirInf in baseDirectory.GetDirectories())
+            {
+                if (dirInf.ToString() == file)
+                {
+                    System.IO.File.Delete(dirInf.FullName);
+                    Directory(baseDirectory);
+                }
+            }
+        }
+
+        private void crypt_Click(object sender, EventArgs e)
+        {
+            string path = "";
+            foreach (FileInfo fi in baseDirectory.GetFiles())
+            {
+                if (fi.ToString() == file)
+                {
+                    path = fi.FullName;
+                }
+            }
+
+            foreach (DirectoryInfo dirInf in baseDirectory.GetDirectories())
+            {
+                if (dirInf.ToString() == file)
+                {
+                    path = dirInf.FullName;
+                }
+            }
+            try
+            {
+                string password = "password";
+                UnicodeEncoding UE = new UnicodeEncoding();
+                byte[] key = UE.GetBytes(password);
+
+                string cryptFile = path + "crypted";
+                FileStream fsCrypt = new FileStream(cryptFile, FileMode.Create);
+
+                RijndaelManaged RMCrypto = new RijndaelManaged();
+
+                CryptoStream cs = new CryptoStream(fsCrypt,
+                    RMCrypto.CreateEncryptor(key, key),
+                    CryptoStreamMode.Write);
+
+                FileStream fsIn = new FileStream(path, FileMode.Open);
+
+                int data;
+                while ((data = fsIn.ReadByte()) != -1)
+                    cs.WriteByte((byte)data);
+
+
+                fsIn.Close();
+                cs.Close();
+                fsCrypt.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Encryption failed!", "Error");
+            }
+            Directory(baseDirectory);
+        }
+    }
+}
